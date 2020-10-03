5a19c679bd9200133d833b43829a2c788791f70f
Bart Janssens <bart@bartjanssens.org>
2020-10-03 17:32:51 +0200

    Update parameters for RMA jupyterhub installation

--- plutoserver/__init__.py
+++ plutoserver/__init__.py
@@ -4,7 +4,7 @@ def setup_plutoserver():
   mydir = os.path.dirname(os.path.realpath(__file__))
   iconpath = os.path.join(mydir, 'icons', 'pluto-logo.svg')
   return {
-    "command": ["julia", "-e", "import Pluto; Pluto.run(\"0.0.0.0\", {port}, configuration=Pluto.ServerConfiguration(launch_browser=false), security=Pluto.ServerSecurity(false))"],
+    "command": ["/opt/julia-1.5/bin/julia", "-e", "import Pluto; Pluto.run(\"0.0.0.0\", {port}, configuration=Pluto.ServerConfiguration(root_url=\"{base_url}/proxy\",launch_browser=false), security=Pluto.ServerSecurity(false))"],
     "timeout": 60,
     "launcher_entry": {
         "title": "Pluto.jl",
