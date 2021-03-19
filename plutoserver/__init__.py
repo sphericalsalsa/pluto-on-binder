eb07b9a0e9a857023303f3302ce9f015b45c6123
sphericalsalsa <73619630+sphericalsalsa@users.noreply.github.com>
2021-03-19 10:54:11 -0400

    update path

--- plutoserver/__init__.py
+++ plutoserver/__init__.py
@@ -4,7 +4,7 @@ def setup_plutoserver():
   mydir = os.path.dirname(os.path.realpath(__file__))
   iconpath = os.path.join(mydir, 'icons', 'pluto-logo.svg')
   return {
-    "command": ["/opt/julia-1.5/bin/julia", "--optimize=0", "-e", "cd(joinpath(ENV[\"HOME\"], \"work\")); import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false)"],
+    "command": ["/opt/julia-1.5/bin/julia", "--optimize=0", "-e", "mkpath(\"/home/jupyter/pluto\"); cd(\"/home/jupyter/pluto\")); import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false)"],
     "timeout": 60,
     "launcher_entry": {
         "title": "Pluto.jl",
